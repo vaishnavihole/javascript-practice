3a4b8326b710dd819ea195b5a1d552bbbc0cb51d
vaishnavihole <vaishnavihole1@gmail.com>
2022-10-21 19:52:01 +0530

    template string

---
+++ java script/map.js
@@ -0,0 +1,20 @@
+// exact copy of for each
+// map return new array
+// for each return undefine
+
+// const students = ["om", "sai", "ram", "sham", "shri"]
+
+// const val = students.map((students , index)=>{
+// console.log("Sending email to", + students )
+
+// })
+// console.log(val)
+
+
+
+
+// templet string
+const studentName = "Suraj"
+const  studentAge = 22
+const message = `${studentName} is ${studentAge} years old`
+console.log(message )
