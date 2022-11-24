d57b16915448652896a86cea82bd0ae505b43fbc
vaishnavihole <vaishnavihole1@gmail.com>
2022-11-24 14:48:24 +0530

    push and pop method of array

--- java script/index.js
+++ java script/index.js
@@ -1,6 +1,49 @@
- const students = ["om", "sai", "ram", "sham", "shri"]
+//  const students = ["om", "sai", "ram", "sham", "shri"]
 
-   students.forEach((item, index)=>{
-   console.log(item, index)
+//    students.forEach((item, index)=>{
+//    console.log(item, index)
     
-})
+// })
+//  let a = 5;
+//  let b= 10;
+//  let ans = a+b;
+//  console.log(ans);
+
+//  console.log('Running NodeJS')
+
+//  const students = ["Aniket", "Jayashree","Pranali", "Avishkar"]
+//  console.log(students)
+
+
+ //JS arrays allows us to store value of different datatypes.
+
+//Access single element in array
+//  const arr = ["Aniket", 1, "Pranali", 8.20]
+//   console.log(arr[0])
+
+
+// Methos of Array
+// 1. Push()
+//          It adds element from end
+
+// example
+
+// const arr = []
+// arr.push("Sakshi")
+// arr.push("Pooja")
+// console.log(arr)
+
+// arr.push("Satyam")
+// arr.push("Pinki")
+// console.log(arr)
+
+
+// 2.POP
+//      It removes last element from array
+
+
+// example
+
+// const arr = ["Satyam", "Pooja", "Avishkar", "Rupal"]
+// arr.pop()
+// console.log(arr)
